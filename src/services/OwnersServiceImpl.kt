1e8dabe487fcdf77a99c1bf2f103ea07dfc0d6b7
Bence Papp <pappbence96@gmail.com>
2020-04-20 19:52:53 +0200

    Added explaining comments

--- src/services/OwnersServiceImpl.kt
+++ src/services/OwnersServiceImpl.kt
@@ -5,13 +5,17 @@ import hu.pappbence.model.PetOwners
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
+/*
+    Service handling pet owner related tasks
+ */
 class OwnersServiceImpl : OwnersService {
+    // List all registered pet owners
     override fun listAll(): List<PetOwnerDto> {
         return transaction {
             PetOwners.selectAll().toList().map{ it.toPetOwnerDto() }
         }
     }
-
+    // Find a pet owner specified by their ID
     override fun findById(id: Int) : PetOwnerDto {
         return transaction { PetOwners.selectAll()
                 .andWhere { PetOwners.id eq id }
@@ -20,6 +24,7 @@ class OwnersServiceImpl : OwnersService {
         }
     }
 
+    // Register a new pet owner
     override fun create(dto: PetOwnerDto): Int {
         val id = transaction {
             PetOwners.insert {
@@ -32,6 +37,7 @@ class OwnersServiceImpl : OwnersService {
         return id.value
     }
 
+    // Update an existing pet owner specified by their ID
     override fun update(id: Int, dto: PetOwnerDto) {
         transaction {
             PetOwners.update({ PetOwners.id eq id }) {
@@ -43,7 +49,9 @@ class OwnersServiceImpl : OwnersService {
         }
     }
 
+    // Map from DB entity to DTO
     private fun ResultRow.toPetOwnerDto() = PetOwnerDto(
+        this[PetOwners.id].value,
         this[PetOwners.name],
         this[PetOwners.phone],
         this[PetOwners.balance],
