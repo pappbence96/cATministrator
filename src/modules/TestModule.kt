2c0929e338ea33f7e0d9163c314d81ba3b1055b8
Bence Papp <pappbence96@gmail.com>
2020-04-20 17:54:04 +0200

    Experimenting with multimodule stuff

--- src/modules/TestModule.kt
+++ src/modules/TestModule.kt
@@ -1,2 +1,15 @@
 package hu.pappbence.modules
 
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun Application.testModule(){
+    routing {
+        get("test"){
+            call.respond("Test successful")
+        }
+    }
+}
