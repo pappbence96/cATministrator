edcef49ac168c1a76d55d33ff87c778730403a08
Bence Papp <pappbence96@gmail.com>
2020-05-20 15:57:59 +0200

    Removed example codes and unnecessary parts

--- src/Application.kt
+++ src/Application.kt
@@ -37,15 +37,15 @@ fun Application.module(testing: Boolean = false) {
             logger = Logger.DEFAULT
             level = LogLevel.HEADERS
         }
-        install(ContentNegotiation){
-            gson{
+        install(ContentNegotiation) {
+            gson {
                 setPrettyPrinting()
                 serializeNulls()
                 setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
                 registerTypeAdapter(DateTime::class.java, JsonJodaTimeAdapter())
             }
         }
-        install(StatusPages){
+        install(StatusPages) {
             exception<NotFoundException> { cause ->
                 call.respond(HttpStatusCode.NotFound, cause.message ?: "")
             }
@@ -57,82 +57,9 @@ fun Application.module(testing: Boolean = false) {
     transaction {
         addLogger(StdOutSqlLogger)
         SchemaUtils.create(PetOwners, Pets, AppointmentTypes, PetAppointmentRegistrations)
-/*
-        AppointmentTypes.insert {
-            it[name] = "Bathing & drying"
-            it[fee] = 5000
-        }
-        AppointmentTypes.insert {
-            it[name] = "Grooming"
-            it[fee] = 6500
-        }
-        AppointmentTypes.insert {
-            it[name] = "Health checkup"
-            it[fee] = 3000
-        }
-        AppointmentTypes.insert {
-            it[name] = "Nail care"
-            it[fee] = 4800
-        }
-        AppointmentTypes.insert {
-            it[name] = "Special fur trimming"
-            it[fee] = 12000
-        }
-        AppointmentTypes.insert {
-            it[name] = "Ear care"
-            it[fee] = 5000
-        }*/
     }
 
-    startKoin{
+    startKoin {
         modules(injectionModule)
     }
-
-    routing {
-        get() {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/html-dsl") {
-            call.respondHtml {
-                body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
-                    }
-                }
-            }
-        }
-
-        get("/styles.css") {
-            call.respondCss {
-                body {
-                    backgroundColor = Color.red
-                }
-                p {
-                    fontSize = 2.em
-                }
-                rule("p.myclass") {
-                    color = Color.blue
-                }
-            }
-        }
-    }
-}
-
-
-fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
-    style(type = ContentType.Text.CSS.toString()) {
-        +CSSBuilder().apply(builder).toString()
-    }
-}
-
-fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
-    this.style = CSSBuilder().apply(builder).toString().trim()
-}
-
-suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
-    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
 }
